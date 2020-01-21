3d6e9c68d4366f9e2aa23a601450fce4e89d28f2
Kris The Human <nikodash2020@hotmail.com>
2020-01-20 19:19:39 -0500

    Updated Bats.... again

--- mp/game/run.bat
+++ mp/game/run.bat
@@ -1,3 +1,3 @@
 @echo off
-	hl2.exe -steam +sv_cheats 1 -game "hl2" -sw -dev -console -allowdebug -condebug -high -conclearlog -noborder -nojoy -force_vendor_id 0x10DE -force_device_id 0x1180 +r_lod 0
+	hl2.exe -steam +sv_cheats 1 -game "mod_hl2mp" -sw -dev -console -allowdebug -condebug -high -conclearlog -noborder -nojoy -force_vendor_id 0x10DE -force_device_id 0x1180 +r_lod 0
 exit
